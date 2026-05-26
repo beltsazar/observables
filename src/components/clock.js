0cf1a74ce95332d87e2d7175aa8cae5053c7a597
Daniel Marcuse <daniel.marcuse@ing.com>
2026-05-26 08:40:26 +0200

    feat: add ClockComponent with context integration for real-time counter display

---
+++ src/components/clock.js
@@ -0,0 +1,52 @@
+import { LitElement, css, html } from "lit";
+import { ContextConsumer } from "@lit/context";
+import { context } from "../context.js";
+
+export class ClockComponent extends LitElement {
+  contextConsumer = new ContextConsumer(this, { context });
+
+  constructor() {
+    super();
+    this.counter = 0;
+  }
+
+  static get properties() {
+    return {
+      counter: { type: Number },
+    };
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.state$ = this.contextConsumer.value.state$;
+    this.subscription = this.state$.observe((data) => {
+      this.counter = data.clock.counter;
+    });
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.subscription.unsubscribe();
+  }
+
+  render() {
+    return html`<div>${this.counter}</div> `;
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: inline-block;
+      }
+
+      div {
+        display: inline-block;
+        min-width: 40px;
+        text-align: center;
+        border: 1px solid #000;
+        border-radius: 6px;
+        padding: 2px;
+      }
+    `;
+  }
+}
