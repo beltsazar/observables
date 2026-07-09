2b481e897e7ad0aa9ea063f7ea1d1a3bdf25e43a
Daniel Marcuse <daniel.marcuse@ing.com>
2026-07-09 12:36:37 +0200

    test: refactor Signal tests for improved clarity and coverage

--- src/lib/signals/core/test/Signal.test.js
+++ src/lib/signals/core/test/Signal.test.js
@@ -1,131 +1,57 @@
-import { expect, describe, it, beforeEach, afterEach } from "vitest";
-import { spy } from "sinon";
-import { cloneDeep, isEqual } from "lodash-es";
+import { expect, describe, it } from "vitest";
 import { Signal } from "../Signal.js";
 
-const state = {};
+const nested = {
+  customer: {
+    name: "Customer",
+  },
+};
 
-describe("ObservableData", () => {
-  let observable$;
-  let event;
-  let callBackSpy;
-  let observerWrapper;
-
-  beforeEach(() => {
-    observable$ = new Signal(cloneDeep(state));
-
-    event = {};
-    observerWrapper = {
-      observer: (data, previousData) => {
-        event = {
-          data,
-          previousData,
-        };
-      },
-    };
-    callBackSpy = spy(observerWrapper, "observer");
-  });
-
-  afterEach(() => {
-    callBackSpy.restore();
-  });
-
+describe("Signal", () => {
   it("should correctly instantiate an observable data object", () => {
-    expect(observable$ instanceof Signal).to.equal(true);
+    expect(new Signal() instanceof Signal).to.equal(true);
   });
 
-  it("should register observer and call it immediately with initial data", () => {
-    observable$.observe(observerWrapper.observer);
-    expect(event.data).to.deep.equal(new Signal(cloneDeep(state)).value);
-    expect(event.previousData).to.deep.equal(null);
-    expect(callBackSpy.calledOnce).to.equal(true);
-
-    observable$.setValue(() => {});
-    expect(callBackSpy.calledTwice).to.equal(true);
-  });
-
-  it("should change a simple property", () => {
-    observable$.observe(observerWrapper.observer);
-
-    observable$.setValue((data) => {
-      data.customer.name = "Daniel Marcuse";
-    });
-    expect(event.data.customer.name).to.equal("Daniel Marcuse");
-    expect(event.previousData.customer.name).to.equal("Daniel");
-    expect(
-      isEqual(event.data.customer.name, event.previousData.customer.name),
-    ).to.equal(false);
-    expect(observable$.value.customer.name).to.equal("Daniel Marcuse");
+  it("should change a simple value", () => {
+    const signal$ = new Signal(0);
+    expect(signal$.value).to.equal(0);
+    signal$.setValue(1);
+    expect(signal$.value).to.equal(1);
+    expect(signal$.previousValue).to.equal(0);
   });
 
   it("should clone objects matching original properties", () => {
-    observable$.observe(observerWrapper.observer);
-
-    observable$.setValue(() => {});
-    expect(event.data.products === event.previousData.products).not.to.equal(
-      true,
-    );
-    expect(event.data.products).to.deep.equal(event.previousData.products);
-  });
-
-  it("should respect deep nested child objects changes", () => {
-    observable$.observe(observerWrapper.observer);
-    expect(observable$.value.options[1].price.amount).to.equal(200);
-    expect(observable$.value.products[0].options[1].price.amount).to.equal(200);
-
-    observable$.setValue((data) => {
-      data.options[1].price.amount = 20;
+    const signal$ = new Signal(nested);
+    expect(signal$.value === nested).to.equal(false);
+    expect(signal$.value).to.deep.equal(nested);
+    expect(signal$.value.customer.name).to.deep.equal("Customer");
+    signal$.setValue((value) => {
+      value.customer.name = "Another Customer";
     });
-    expect(event.data.products === event.previousData.products).not.to.equal(
-      true,
-    );
-    expect(event.data.products).not.to.deep.equal(event.previousData.products);
-    expect(isEqual(event.data.products, event.previousData.products)).to.equal(
-      false,
-    );
-    expect(
-      isEqual(event.data.products[0], event.previousData.products[0]),
-    ).to.equal(false);
-    expect(
-      isEqual(event.data.products[2], event.previousData.products[2]),
-    ).to.equal(true);
-
-    expect(observable$.value.options[1].price.amount).to.equal(20);
-    expect(observable$.value.products[0].options[1].price.amount).to.equal(20);
+    expect(signal$.value.customer.name).to.equal("Another Customer");
+    expect(signal$.previousValue).to.deep.equal(nested);
   });
 
-  it("should be able to unsubscribe", () => {
-    const subscription1 = observable$.observe(observerWrapper.observer);
-    const subscription2 = observable$.observe(observerWrapper.observer);
+  it("should only notify when the new value is not equal to the previous one", () => {
+    const signal$ = new Signal(nested);
+    const oldValue = signal$.value;
+    signal$.setValue(nested);
+    expect(signal$.value === oldValue).to.equal(true);
 
-    expect(callBackSpy.callCount).to.equal(2);
-
-    observable$.setValue(() => {});
-    expect(callBackSpy.callCount).to.equal(4);
-
-    subscription1.unsubscribe();
-    observable$.setValue(() => {});
-    expect(callBackSpy.callCount).to.equal(5);
-
-    subscription2.unsubscribe();
-    observable$.setValue(() => {});
-    expect(callBackSpy.callCount).to.equal(5);
+    signal$.setValue((value) => {
+      value.customer.name = "Another Customer";
+    });
+    expect(signal$.value === oldValue).to.not.equal(true);
   });
 
   it("should be immutable!!!", () => {
+    const signal$ = new Signal(0);
+    const signalWithNestedObject$ = new Signal(nested);
     expect(() => {
-      observable$.value.options[1].price.amount = 20;
+      signal$.value = 20;
     }).to.throw(Error);
     expect(() => {
-      observable$.value.products[0] = {};
+      signalWithNestedObject$.customer.name = "Another Customer";
     }).to.throw(Error);
-  });
-
-  it("should call an object method", () => {
-    observable$.observe(observerWrapper.observer);
-    observable$.setValue(() => {});
-    expect(
-      observable$.value.products[0].hasOption(observable$.value.options[1]),
-    ).to.equal(true);
   });
 });
